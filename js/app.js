09448c79aef57f2e639a1d36e662b6c6af4a75d2
biancio93 <106167055+biancio93@users.noreply.github.com>
2023-10-10 15:57:35 +0200

    first svg animation commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let circle = document.getElementById('circle');
+let path = document.getElementById('path');
+
+function animationHover(){
+    circle.classList.add('animation-circle');
+    path.classList.add('animation-path');
+}
